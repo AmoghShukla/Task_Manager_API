dc5fc61f927c02bd3c2e6d41ce6c5e7b18b4c578
Amogh Shukla <amoghshukla548@gmail.com>
2026-03-20 23:56:38 +0530

    update task service to improve logging and structure

--- service/task_service.py
+++ service/task_service.py
@@ -1,7 +1,8 @@
 from repository.task_repository import db
-from utils.Task_logger import logger
+from utils.Task_logger import get_logger
+from fastapi import HTTPException
 
-logger = get_lo
+logger = get_logger(__name__)
 
 class Task_Service:
 
@@ -14,16 +15,39 @@ class Task_Service:
         }
 
         db.Database[db.Counter] = task
+        logger.info("Task Creation Successful!!")
+        db.Counter += 1
         
     
     def Get_All_Task():
-        return repository
+        logger.info("Fetching all Data from the Database")
+        return list(db.Database.values())
+
     
     def Get_Task(task_id):
-        return repository
+        logger.info(f"Getching the Task with Task Id : {task_id}")
+        return db.Database[task_id]
     
-    def Update_Task(task_id):
-        return repository
+    def Update_Task(task_id: int, data):
+        if task_id in db.Database:
+            task = db.Database.get(task_id)
+            logger.info(f"Updating Task with Task ID : {task_id}")
+            if not task:
+                return None
+
+            if data.title is not None:
+                task["title"] = data.title
+            if data.description is not None:
+                task["description"] = data.desscription
+            return task
+        else:
+            raise HTTPException(status_code=404, detail="Task Not Found")
     
     def Delete_Task(task_id):
-        return repository
+        if task_id in db.Database:
+            logger.info(f"Deleting Entry with taks_id {task_id}")
+            del db.Database[task_id]
+            return "Task Deleted Successfully"
+        else:
+            return HTTPException(status_code=404, detail="Task Not Found")
+
